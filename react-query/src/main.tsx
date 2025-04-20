8e77ebf1c3a2c8694772ae0ebca4d37f3f86b4b5
Youssef <youssefbenlemlih7@gmail.com>
2025-04-20 13:58:00 +0200

    implement 14, 15

--- react-query/src/main.tsx
+++ react-query/src/main.tsx
@@ -25,6 +25,10 @@ import Pattern9 from "./patterns/09-query-key-factories";
 import Pattern10 from "./patterns/10-simple-mutations";
 import Pattern11 from "./patterns/11-query-invalidation";
 import Pattern12 from "./patterns/12-automatic-query-invalidation";
+import Pattern13 from "./patterns/13-global-error-handling";
+import Pattern14 from "./patterns/14-optimistic-updates-in-ui";
+import Pattern15 from "./patterns/15-optimistic-updates-in-cache";
+import Pattern16 from "./patterns/16-suspense-queries";
 import { Patterns } from "./patterns/Patterns";
 
 const rootRoute = createRootRoute({
@@ -36,64 +40,84 @@ const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
 });
 const pattern1 = createRoute({
-  path: "/01",
+  path: "/01-simple-queries",
   getParentRoute: () => rootRoute,
   component: Pattern1,
 });
 const pattern2 = createRoute({
-  path: "/02",
+  path: "/02-custom-queries",
   getParentRoute: () => rootRoute,
   component: Pattern2,
 });
 const pattern3 = createRoute({
-  path: "/03",
+  path: "/03-selectors",
   getParentRoute: () => rootRoute,
   component: Pattern3,
 });
 const pattern4 = createRoute({
-  path: "/04",
+  path: "/04-parameterized-queries",
   getParentRoute: () => rootRoute,
   component: Pattern4,
 });
 const pattern5 = createRoute({
-  path: "/05",
+  path: "/05-disabling-queries",
   getParentRoute: () => rootRoute,
   component: Pattern5,
 });
 const pattern6 = createRoute({
-  path: "/06",
+  path: "/06-pagination",
   getParentRoute: () => rootRoute,
   component: Pattern6,
 });
 const pattern7 = createRoute({
-  path: "/07",
+  path: "/07-prefetching",
   getParentRoute: () => rootRoute,
   component: Pattern7,
 });
 const pattern8 = createRoute({
-  path: "/08",
+  path: "/08-infinite-queries",
   getParentRoute: () => rootRoute,
   component: Pattern8,
 });
 const pattern9 = createRoute({
-  path: "/09",
+  path: "/09-query-key-factories",
   getParentRoute: () => rootRoute,
   component: Pattern9,
 });
 const pattern10 = createRoute({
-  path: "/10",
+  path: "/10-simple-mutations",
   getParentRoute: () => rootRoute,
   component: Pattern10,
 });
 const pattern11 = createRoute({
-  path: "/11",
+  path: "/11-query-invalidation",
   getParentRoute: () => rootRoute,
   component: Pattern11,
 });
 const pattern12 = createRoute({
-  path: "/12",
+  path: "/12-automatic-query-invalidation",
   getParentRoute: () => rootRoute,
   component: Pattern12,
+});
+const pattern13 = createRoute({
+  path: "/13-global-error-handling",
+  getParentRoute: () => rootRoute,
+  component: Pattern13,
+});
+const pattern14 = createRoute({
+  path: "/14-optimistic-updates-in-ui",
+  getParentRoute: () => rootRoute,
+  component: Pattern14,
+});
+const pattern15 = createRoute({
+  path: "/15-optimistic-updates-in-cache",
+  getParentRoute: () => rootRoute,
+  component: Pattern15,
+});
+const pattern16 = createRoute({
+  path: "/16-suspense-queries",
+  getParentRoute: () => rootRoute,
+  component: Pattern16,
 });
 const routeTree = rootRoute.addChildren([
   indexRoute,
@@ -109,6 +133,10 @@ const routeTree = rootRoute.addChildren([
   pattern10,
   pattern11,
   pattern12,
+  pattern13,
+  pattern14,
+  pattern15,
+  pattern16,
 ]);
 
 // Set up a Router instance
